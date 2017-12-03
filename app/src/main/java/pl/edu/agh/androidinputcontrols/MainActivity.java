efdafc34b536bc7f7b1343b96ae7ecb4112f33de
wojtas626 <wojtas626@gmail.com>
2017-12-03 13:47:24 +0100

    zad1

--- app/src/main/java/pl/edu/agh/androidinputcontrols/MainActivity.java
+++ app/src/main/java/pl/edu/agh/androidinputcontrols/MainActivity.java
@@ -2,6 +2,9 @@ package pl.edu.agh.androidinputcontrols;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showPassword(View view) {
+        EditText passwordField = findViewById(R.id.passwordField);
+        String currentPassword = passwordField.getText().toString();
+        TextView passwordView = findViewById(R.id.passwordView);
+        passwordView.setText(currentPassword);
     }
 }
